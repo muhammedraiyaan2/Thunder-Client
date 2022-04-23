0d745f90f6638ed4413c6725fd9d2f0d73f4e7c1
muhammedraiyaan2 <muhammedraiyaan2@gmail.com>
2022-04-23 23:30:01 +0530

    The site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from 'react'
+import Navbar from './Navbar'
 function App() {
+  useEffect(() => {
+    let gets=document.getElementById("get")
+  let getsBtn=document.getElementById("getBtn")
+  let output=document.getElementById("output")
+  getsBtn.addEventListener("click",function(e) {
+    fetch(gets.value).then(function(response){
+        return response.text()
+    }).then(function(responses){
+      output.innerHTML=responses
+    })
+  })
+  }, )
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div className="form">
+      <div className="form2">
+      <input type="text" id="get" />
+      <button id="getBtn">GET</button>
+      <pre className="language-html">
+        <code id="output"></code>
+      </pre>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+export default function Navbar() {
+  return (
+    <>
+    <nav>
+      <li className="head">Thunder Client</li>
+     <ul>
+      <li>Get Request</li>
+      <li>Post Request</li>
+     </ul>
+    </nav>
+    </>
+  )
+}
